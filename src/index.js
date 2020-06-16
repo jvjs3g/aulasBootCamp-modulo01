78354eab1372893d0534f0cb2d6334c8a79b4fef
joao vitor <joao3klb@hotmail.com>
2020-06-15 21:19:36 -0300

    Middlewares

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { uuid } =  require('uuidv4');
+const { uuid, isUuid } =  require('uuidv4');
 
 const app = express();
 app.use(express.json());
@@ -7,6 +7,27 @@ app.use(express.json());
 
 
 const projects = [];
+
+function logRequests(request,response,next){
+  const { method, url } = request;
+
+  const logLabel =  `[${method.toUpperCase()}] ${url}`;
+
+  console.log(logLabel);
+  return next();
+}
+
+
+function validateProjectsId(request,response,next){
+  const { id } = request.params;
+  if(!isUuid(id)){
+    return response.status(400).json({error:'Invalid project Id.'});
+  }
+
+  return next();
+}
+app.use(logRequests);
+app.use('/projects/:id', validateProjectsId);
 
 app.get('/projects', (request,response) =>{
 
